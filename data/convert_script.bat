3987213dd2c6b4ade7a36d74021413403ea865c0
vkoskinen <koskinen.vesa@outlook.com>
2022-02-19 16:56:53 +0200

    Performance enhancements

--- data/convert_script.bat
+++ data/convert_script.bat
@@ -2,4 +2,5 @@ call curl -d "@q1.json" -X POST https://overpass-api.de/api/interpreter -o cafes
 call curl -d "@q2.json" -X POST https://overpass-api.de/api/interpreter -o fuels.json
 call osmtogeojson fuels.json > fuel.geojson
 call osmtogeojson cafes.json > cafe.geojson
-
+call minify-geojson -w "name, url, website, opening_hours" C:\vkoskinen.github.io\data\cafe.geojson
+call minify-geojson -w "name, url, website, opening_hours" C:\vkoskinen.github.io\data\fuel.geojson
